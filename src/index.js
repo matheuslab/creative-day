c2474e0730f3ac48ff69379c115e4790ebbcfcea
matheuslab <matheus.martins@cobli.co>
2021-07-01 12:29:28 -0300

    heroku integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(routes);
 
-const port = 3333;
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3333;
 
 app.listen(port);
